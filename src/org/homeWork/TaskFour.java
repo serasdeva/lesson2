9b1ac1ba2ba0a26b94e64b05c07f15c9072b19e9
serasdeva <serasdeva@gmail.com>
2022-12-03 11:15:00 +0700

    TaskFour - Четвертое задание + класс TreeNode

---
+++ src/org/homeWork/TaskFour.java
@@ -0,0 +1,35 @@
+package org.homeWork;
+
+public class TaskFour {
+    TreeNode root;
+
+    public static void main(String[] args) {
+        TaskFour p = new TaskFour();
+
+        p.root = new TreeNode(1);
+        p.root.left = new TreeNode(2);
+        p.root.right = new TreeNode(3);
+        p.root.left.left = new TreeNode(5);
+
+        TaskFour q = new TaskFour();
+        q.root = new TreeNode(1);
+        q.root.left = new TreeNode(2);
+        q.root.right = new TreeNode(3);
+        q.root.left.left = new TreeNode(5);
+
+        isSameTree(p.root, q.root);
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) return true;
+
+        if (p == null || q == null) return false;
+
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+
+    }
+}
